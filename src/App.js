bca2d99e1ef17f5af985bb75abb93950404d0c6f
2002Sanjai <113833007+2002Sanjai@users.noreply.github.com>
2024-03-11 16:47:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
-// import Accordion from './components/Accordion'
+import Accordion from './components/Accordion'
 import RandomColor from './components/RandomColor'
 function App() {
   return (
     <div className="App">
       {/* Accordion Component*/}
-      {/* {
+      {
         <Accordion/>
-      } */}
+      }
+  { /* Random Color*/}
       {
         <RandomColor/>
       }
